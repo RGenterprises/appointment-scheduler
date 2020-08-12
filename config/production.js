cdefeb11c4089d417e494260ce6ff464fcf90c5d
ryan.kuenneke@outlook.com <ryan@ryans-MacBook-Pro.local>
2020-08-12 14:35:04 -0400

    update prod config

--- config/production.js
+++ config/production.js
@@ -1,12 +1,7 @@
 module.exports = {
-  bucket: {
-    slug: process.env.COSMIC_BUCKET,
-    read_key: process.env.COSMIC_READ_KEY,
-    write_key: process.env.COSMIC_WRITE_KEY
-  },
   twilio: {
-    auth: process.env.TWILIO_AUTH,
-    sid: process.env.TWILIO_SID,
-    number: process.env.TWILIO_NUMBER
+    auth: 'b4a993b2aca6c9462f132b07f0cb77f4',
+    sid: 'AC50f7fa164716b6e756cea6891d057bff',
+    number: '+18559573999'
   }
 }
