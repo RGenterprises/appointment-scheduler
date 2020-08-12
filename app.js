5c682fa643786421328f3d7414611c46da7a6807
ryan.kuenneke@outlook.com <ryan@ryans-MacBook-Pro.local>
2020-08-12 13:28:13 -0400

    eb deploy configs

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.set('port', process.env.PORT || 3000)
 
 //handle requests for new appointments
